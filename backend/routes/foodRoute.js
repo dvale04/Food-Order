6b19644a66cc0bb38af488a780cb1657a861a5b2
dvale04 <84812619+dvale04@users.noreply.github.com>
2026-04-12 16:29:26 -0600

    add post

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -4,6 +4,9 @@ import multer from "multer"
 
 const foodRouter = express.Router();
 
+foodRouter.post("/add",addFood) //send the data on the server and on the server
+//data will be processed and get response from the server  ex. when fill a form,
+//that form data will be sent using the post method 
 
 
 
