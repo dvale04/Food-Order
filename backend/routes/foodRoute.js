c987f988a563d028391279b81c7099b47d27fb1d
dvale04 <84812619+dvale04@users.noreply.github.com>
2026-04-16 20:59:06 -0600

    add post method

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -4,7 +4,18 @@ import multer from "multer"
 
 const foodRouter = express.Router();
 
-foodRouter.post("/add",addFood) //send the data on the server and on the server
+// Image storage Engine 
+
+const storage = multer.diskStorage({
+    destination:"uploads",
+    filename:(req,file,cb)=>{
+        return cb(null,`${Date.now()}${file.originalname}`)
+    }
+})
+const upload = multer({storage:storage})
+
+
+foodRouter.post("/add",upload.single("image"),addFood) //send the data on the server and on the server
 //data will be processed and get response from the server  ex. when fill a form,
 //that form data will be sent using the post method 
 
