ddc57d5312eb0acf214acdd7d0b5c17d1bcb9bcf
dvale04 <84812619+dvale04@users.noreply.github.com>
2026-05-04 08:04:12 -0600

    add listFood logic

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -28,6 +28,15 @@ const addFood = async (req,res) => {
 
 // all food list
 const listFood = async (req,res) => {
+    try {
+        const foods = await foodModel.find({});
+        res.json({success:true,data:foods})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+
+    }
 
 }
 
